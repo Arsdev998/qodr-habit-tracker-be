ace44d7ff99fa074a0029105b21ce3a8559cf2d5
Arsdev998 <arsdev998@gmail.com>
2024-11-19 01:58:32 +0700

    test fix res-cookie 4 add vercel domain

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -38,6 +38,7 @@ export class AuthController {
       secure: process.env.NODE_ENV === 'production', 
       sameSite: 'none',
       expires: new Date(Date.now() + 3600000),
+      domain:'.vercel.app',
       path: '/', 
     });
     return user;
