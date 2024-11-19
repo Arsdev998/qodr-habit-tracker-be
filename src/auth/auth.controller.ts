81b7b5beebb4cd548bb559f40e4a259a212220f6
Arsdev998 <arsdev998@gmail.com>
2024-11-19 15:18:39 +0700

    fix res cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -41,7 +41,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.PRODUCTION === 'production',
       maxAge: expiresIn * 1000, // Convert ke milliseconds
-      sameSite: 'none',
+      sameSite: 'strict',
       path: '/',
     });
 
