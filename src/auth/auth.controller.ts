ae1991b03e7e759dc6db4128b5f2d23aba39d6ab
Arsdev998 <arsdev998@gmail.com>
2024-11-19 08:08:50 +0700

    test sameSite strict

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
     response.cookie('jwt', access_token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'lax',
+      sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000,
       path: '/',
     });
