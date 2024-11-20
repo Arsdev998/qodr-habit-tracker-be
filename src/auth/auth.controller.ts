785b06e6f923ced7b809ef473971bf03ac036d8d
Arsdev998 <arsdev998@gmail.com>
2024-11-20 08:53:29 +0700

    test remove header

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -39,9 +39,6 @@ export class AuthController {
       expires.getMilliseconds() +
         ms(this.configService.getOrThrow<string>('JWT_EXPIRATION')),
     );
-    // Tambahkan Bearer token ke header
-    response.setHeader('Access-Control-Expose-Headers', 'authorization');
-    response.setHeader('Authorization', `Bearer ${access_token}`);
     // Tetap set cookie untuk fallback
     response.cookie('jwt', access_token, {
       httpOnly: true,
